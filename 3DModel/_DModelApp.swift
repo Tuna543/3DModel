98ea49a53b9822066c43951b2ee9fc72e2ca06a7
Tuna543 <fairuzahmed13579@gmail.com>
2024-08-29 21:14:57 +0600

    updating

---
+++ 3DModel/_DModelApp.swift
@@ -0,0 +1,19 @@
+//
+//  _DModelApp.swift
+//  3DModel
+//
+//  Created by BS00880 on 19/8/24.
+//
+
+import SwiftUI
+
+@main
+struct _DModelApp: App {
+    static let subsystem: String = "me.fairuz.ahmed.-DModel"
+//    com.example.apple-samplecode.GuidedCapture${SAMPLE_CODE_DISAMBIGUATOR}
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
